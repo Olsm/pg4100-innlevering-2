c9e559ae109b6b5bcbf9e85bf548d695202f41d4
OlavOlsm <OlavOlsm@gmail.com>
2016-02-17 19:27:37 +0100

    create constructor code

--- Client.java
+++ Client.java
@@ -1,4 +1,19 @@
 package innlevering2;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
+	Socket connection;
+	DataOutputStream output;
+	DataInputStream input;
+
+	public Client(String address, int port) throws IOException {
+		connection = new Socket(address, port);
+		output = new DataOutputStream(connection.getOutputStream());
+		input = new DataInputStream(connection.getInputStream());
+	}
+
 }
