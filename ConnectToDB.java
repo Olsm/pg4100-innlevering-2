9a8b40c192527cdb5036a474dc4d0780ff62791b
OlavOlsm <OlavOlsm@gmail.com>
2016-02-17 19:09:22 +0100

    add ConnectToDB code

--- ConnectToDB.java
+++ ConnectToDB.java
@@ -1,4 +1,26 @@
 package innlevering2;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectToDB {
+    private Connection con;
+
+    // Connect to database using DriverManager.getConnection
+    public ConnectToDB (String serverName, String databaseName,
+                        String user, String password) throws SQLException {
+        con = DriverManager.getConnection("jdbc:mysql://" +
+                serverName + "/" + databaseName, user, password);
+    }
+
+    // Close the database connection
+    public void close() throws SQLException {
+        con.close();
+    }
+
+    // Return the database connection
+    public Connection getConnection() {
+        return con;
+    }
 }
