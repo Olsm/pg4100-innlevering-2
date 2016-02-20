4456a2c233eaf34280a8bbdb5de3d422347ce9b6
OlavOlsm <OlavOlsm@gmail.com>
2016-02-20 13:28:53 +0100

    add unit tests for DBHandlerBokliste

--- DBHandlerBoklisteTest.java
+++ DBHandlerBoklisteTest.java
@@ -4,22 +4,41 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class DBHandlerBoklisteTest {
 
-	@Before
-	public void setUp() throws Exception {
+	DBHandlerBokliste dbHB;
 
+	@Before
+	public void setUp() {
+		try {
+			dbHB = new DBHandlerBokliste("localhost", "root", "root");
+		} catch (SQLException e) {
+			fail("SQLException: " + e.getMessage());
+		}
 	}
 
 	@After
-	public void tearDown() throws Exception {
-
+	public void tearDown() {
+		try {
+			dbHB.close();
+		} catch (SQLException e) {
+			fail("SQLException: " + e.getMessage());
+		}
 	}
 
 	@Test
-	public void testGetTable() throws Exception {
-
+	public void testGetTable() {
+		try {
+			ArrayList<String> table = dbHB.getTable();
+			assertEquals("ISBN|forfatter|tittel", table.get(0));
+			assertTrue(table.contains("0-099-30278-0|DIAMOND, JARED|GUNS, GERMS AND STEEL"));
+		} catch (SQLException e) {
+			fail("SQLException: " + e.getMessage());
+		}
 	}
 }
